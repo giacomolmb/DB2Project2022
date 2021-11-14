28c86f5df131013248b4947dacf0d8af410e06fb
giacomolmb <giacomo.lombardo@mail.polimi.it>
2021-11-14 17:50:05 +0100

    EJB module setup

---
+++ TelcoEJB/src/main/java/it/polimi/db69/telco/telcoejb/entities/Order.java
@@ -0,0 +1,79 @@
+package it.polimi.db69.telco.telcoejb.entities;
+
+import it.polimi.db69.telco.telcoejb.enums.OrderStatus;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "order")
+public class Order{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false)
+    private int id;
+
+    @Column(name = "datetime", nullable = false)
+    private Timestamp datetime;
+
+    @Column (name = "status", nullable = false, length = 45)
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    private User orderUser;
+
+    @OneToOne(mappedBy = "subOrder")
+    private Subscription orderSubscription;
+
+    public Order() {
+    }
+
+    public Order(int id, Timestamp datetime, OrderStatus status, User orderUser, Subscription orderSubscription) {
+        this.id = id;
+        this.datetime = datetime;
+        this.status = status;
+        this.orderUser = orderUser;
+        this.orderSubscription = orderSubscription;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Timestamp getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Timestamp datetime) {
+        this.datetime = datetime;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getOrderUser() {
+        return orderUser;
+    }
+
+    public void setOrderUser(User orderUser) {
+        this.orderUser = orderUser;
+    }
+
+    public Subscription getOrderSubscription() {
+        return orderSubscription;
+    }
+
+    public void setOrderSubscription(Subscription orderSubscription) {
+        this.orderSubscription = orderSubscription;
+    }
+}
---
+++ TelcoEJB/src/main/java/it/polimi/db69/telco/telcoejb/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package it.polimi.db69.telco.telcoejb.enums;
+
+public enum OrderStatus {
+    PENDING,
+    ACCEPTED,
+    REJECTED
+}
