fdcbe3de63bcf4990c0ceae0d8c0edea4446d625
valeria <valeria.detomas@mail.polimi.it>
2021-11-26 19:07:24 +0100

    small fix

---
+++ TelcoWEB/src/main/java/it/polimi/db69/telco/telcoweb/controllers/ConfirmationServlet.java
@@ -0,0 +1,9 @@
+package it.polimi.db69.telco.telcoweb.controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "ConfirmationServlet", value = "/confirmation")
+public class ConfirmationServlet extends HttpServlet {
+
+}
